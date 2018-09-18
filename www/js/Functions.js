954f7c1280d79a96f9f2759b28419772e48e9e89
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-18 17:34:11 +0530

    add

--- www/js/Functions.js
+++ www/js/Functions.js
@@ -20,6 +20,17 @@ document.addEventListener("deviceready",onDeviceReady,false);
           alert("you need to download barcodescanner" + error);
       }
     }*/  
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
 					   
 
 	}

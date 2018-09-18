eb98f6fe668e318637e344347d21daf9bec3a2c9
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-18 17:52:17 +0530

    test3

--- www/js/Functions.js
+++ www/js/Functions.js
@@ -1,14 +1,6 @@
 document.addEventListener("deviceready",onDeviceReady,false);
 	function onDeviceReady(){
 		alert("device is starting");
-	/*cordova.plugins.barcodeScanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-    );*/
-    /*function scanBarCode(){
-		alert('barcode scanner is starting');
 	      cordova.plugins.barcodeScanner.scan(
 	      function (result) {
           alert("Barcode found\n" +
@@ -19,24 +11,8 @@ document.addEventListener("deviceready",onDeviceReady,false);
       function (error) {
           alert("you need to download barcodescanner" + error);
       }
-    }*/  
-    cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
-   cordova.plugins.barcodeScanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-        );
-					   
+      );
+  
+    
 
 	}

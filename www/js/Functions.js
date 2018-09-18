54d553243cdd456d6befb4a6c3710d8f3c6f9922
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-18 18:25:15 +0530

    test6

--- www/js/Functions.js
+++ www/js/Functions.js
@@ -13,7 +13,7 @@ document.addEventListener("deviceready",onDeviceReady,false);
       }
       );*/
   
-    document.getElementById("btn1").addEventListener("click",cameraTakePicture);
+    //document.getElementById("btn1").addEventListener("click",cameraTakePicture);
     function cameraTakePicture(){
 		alert("camera is starting");
 		alert("camera :" +navigator.camera);

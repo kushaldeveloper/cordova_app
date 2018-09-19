f374a00756d79b4b4ff5a004585fa1ccde41d267
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-19 11:55:56 +0530

    testtt2

--- www/js/Functions.js
+++ www/js/Functions.js
@@ -1,7 +1,7 @@
 document.addEventListener("deviceready",onDeviceReady,false);
 	function onDeviceReady(){
 		alert("device is starting");
-	     /* cordova.plugins.barcodeScanner.scan(
+	      cordova.plugins.barcodeScanner.scan(
 	      function scanBarCode(result) {
           alert("Barcode found\n" +
                 "Access" + result.text + "\n" +
@@ -11,26 +11,6 @@ document.addEventListener("deviceready",onDeviceReady,false);
       function scanBarCode(error) {
           alert("you need to download barcodescanner" + error);
       }
-      );*/
-  
-    document.getElementById("btn1").addEventListener("click",cameraTakePicture);
-    function cameraTakePicture(){
-		alert("camera is starting");
-		alert("camera :" +navigator.camera);
-		alert("camera :"+ navigator.camera.getPicture);
-						navigator.camera.getPicture(onSuccess, onFail, {  
-						  quality: 50, 
-						  destinationType: Camera.DestinationType.DATA_URL 
-					   }); 
-				   }			    
-						function onSuccess(imageData) { 
-						  alert('sucess');
-						  var image = document.getElementById('myImage'); 
-						  image.src = "data:image/jpeg;base64," + imageData; 
-					   } 
-						function onFail(message) { 
-						  alert('Failed because: ' + message); 
-					   }
-					   
+      );
 
 	}
